e43bdf16731de3a0400ce3b06fc3c36d45ab6333
Oujiee <Oujieesama@gmail.com>
2022-11-02 09:24:30 +0700

    002ori

---
+++ config.js
@@ -0,0 +1,69 @@
+/** 
+
+
+Base : Hisoka
+Recode : Naze & Oujiee
+
+**/
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+// Website Api
+global.APIs = {
+	zenz: 'https://zenzapis.xyz',
+}
+
+// Apikey Website Api
+global.APIKeys = {
+	'https://zenzapis.xyz': '0e92565522',
+}
+
+// Other
+global.owner = ['6283833771579']
+global.ownernomer = "6283833771579"
+global.premium = ['6283833771579']
+global.packname = '002 Pack'
+global.author = 'Oujiee'
+global.sessionName = '002'  //jangan diganti bro nanti error
+global.jumlha = '999'
+global.jumhal = '100000000000000'
+global.jumlah = '1000000000'
+global.prefa = ['','!','.','#','&']
+global.sp = ''
+global.mess = {
+    success: '✅Done',
+    admin: 'Fitur Khusus Admin Group!',
+    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+    premime: 'Fitur Khusus Premium Kalo Mau Daftar Ketik Sewa',
+    owner: 'Fitur Khusus Owner Bot',
+    group: 'Fitur Digunakan Hanya Untuk Group!',
+    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+    bot: 'Fitur Khusus Pengguna Nomor Bot',
+    wait: 'Loading...',
+    errapi: 'Error Mungkin Apikey Tidak Valid!',
+    errmor: 'Error Kesalahan Sistem',
+    endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
+}
+global.limitawal = {
+    premium: "Infinity",
+    free: 10
+}
+global.thumb = fs.readFileSync('./media/image/naze.jpg')
+global.faall = fs.readFileSync('./media/image/fake.jpg')
+global.mygit = 'https://github.com/Oujiee'
+global.myyt = 'Oujiee'
+global.myytv = 'Oujiee'
+global.mygc = "-"
+global.botname = 'Codename 002'
+global.akulaku = 'Bot By Unknown'
+global.ytname = 'Oujiee'
+
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
